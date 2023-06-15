8529e36f322d78572f3b2e054ee0a41681572d8b
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-15 11:33:47 +0200

    Begin

---
+++ src/main/java/com/ocr/mediscreen_ui/Mediscreen_UIApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.mediscreen_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mediscreen_UIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mediscreen_UIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocr/mediscreen/MediscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocr.mediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
