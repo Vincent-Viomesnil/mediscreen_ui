d18927e37bd13bb2c45a46ff2892eae28dc14e6e
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-22 13:44:36 +0200

    documentation API SWAGGER

--- src/main/java/com/ocr/mediscreen_ui/Mediscreen_UIApplication.java
+++ src/main/java/com/ocr/mediscreen_ui/Mediscreen_UIApplication.java
@@ -3,9 +3,11 @@ package com.ocr.mediscreen_ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients("com.ocr.mediscreen_ui")
+@EnableSwagger2
 public class Mediscreen_UIApplication {
 
 	public static void main(String[] args) {
