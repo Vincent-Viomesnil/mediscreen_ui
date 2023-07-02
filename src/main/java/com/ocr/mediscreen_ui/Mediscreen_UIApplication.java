b8ac7b181ee0ba74f81c0ba967c4487251e94061
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-07-02 20:07:43 +0200

    adding modules - working

--- src/main/java/com/ocr/mediscreen_ui/Mediscreen_UIApplication.java
+++ src/main/java/com/ocr/mediscreen_ui/Mediscreen_UIApplication.java
@@ -3,7 +3,6 @@ package com.ocr.mediscreen_ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
